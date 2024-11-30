4a2e9811e50d60c730f4d0a12d765596f90c3572
majeurbilly <billy.halle.bh@gmail.com>
2024-11-30 14:25:25 -0500

    plus que le soucis du message d'erreur qui se point durant 1 seconde..

--- BilHal - devoir-cours-14/js/exercice_2_main.js
+++ BilHal - devoir-cours-14/js/exercice_2_main.js
@@ -3,17 +3,22 @@ const bonaire = document.getElementById("bonaire");
 const grandTurk = document.getElementById("grandturk");
 const checkboxDiv = document.getElementById("checkboxDiv");
 const formulaireInscription = document.getElementById("formulaireInscription");
+const messageErreur = document.getElementById("messageErreur");
+formulaireInscription.onsubmit = ValiderFormulaire_exercice2;
 
-function ValiderFormulaire_exercice2() {
+function ValiderFormulaire_exercice2(evenment) {
     let nbChampsInvalides = 0;
     if (!CheckBoxIsValid())
     {
         ++nbChampsInvalides;
-        checkboxDiv.classList.add('border-danger');
-        messageErreur.textContent += "Rien de cocher";
+        belize.classList.add('border-danger');
+        bonaire.classList.add('border-danger');
+        grandTurk.classList.add('border-danger');
+        messageErreur.textContent += "T'as rien de coché";
     } else {
-        number1.classList.remove('border-danger');
-        number2.classList.remove('border-danger');
+        belize.classList.remove('border-danger');
+        bonaire.classList.remove('border-danger');
+        grandTurk.classList.remove('border-danger');
     }
     if (nbChampsInvalides !== 0) {
         messageErreur.classList.remove('d-none');
@@ -23,9 +28,6 @@ function ValiderFormulaire_exercice2() {
         alert('Formulaire envoyé')
     }
 }
-
-formulaireInscription.onsubmit = ValiderFormulaire_exercice2;
-
 function CheckBoxIsValid() {
     let temp = 0;
     if (belize.value !== null)
